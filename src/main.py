5faf8c172db0ceeb042458742fe5a876182cac25
Andrew Wingate <1464801+anfroholic@users.noreply.github.com>
2023-06-07 04:49:34 -0500

    add jinja stuff and pics

--- src/main.py
+++ src/main.py
@@ -4,7 +4,9 @@ from fastapi import FastAPI
 
 from fastapi.responses import HTMLResponse
 from datetime import datetime
-
+from fastapi import FastAPI, Form, Request, File
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 # import debugpy
 
@@ -12,7 +14,8 @@ from datetime import datetime
 # print("Waiting for client to attach...")
 # debugpy.wait_for_client()
 app = FastAPI()
-       
+app.mount("/static", StaticFiles(directory="src/static", html=True), name="static")
+templates = Jinja2Templates(directory='src/htmldirectory')    
 
 @app.on_event("startup")
 async def startup_event():
@@ -21,11 +24,14 @@ async def startup_event():
 
 
 @app.get("/", response_class=HTMLResponse)
-def read_root():
-    return f"""
-    <h1>Santa Site for Mark Wingate</h1><br>
-    <strong>The current time is: {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}
-    """
+async def home(request: Request):
+    return templates.TemplateResponse('index.html', {'request': request, 'date': datetime.now().strftime("%Y-%m-%d %H:%M:%S")})
 
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=80, log_level="info")
+# def read_root():
+#     return f"""
+#     <h1>Santa Site for Mark Wingate</h1><br>
+#     <strong>The current time is: {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}
+#     """
+
+# if __name__ == "__main__":
+#     uvicorn.run("main:app", host="0.0.0.0", port=80, log_level="info")
