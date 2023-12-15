0a6fd843af2338176b1b60e5a5462e0123a940c8
Andrew Wingate <1464801+anfroholic@users.noreply.github.com>
2023-12-15 03:50:34 -0600

    add pics

--- src/choose_adventure.py
+++ src/choose_adventure.py
@@ -2,37 +2,48 @@
 story = {
     'Greeting':[ 
         {"name": "Merry Christmas",
-         "snippet": "Merry Cristmas to everyone!"}, 
+         "snippet": "Merry Cristmas to everyone!",
+         "pic": "see_no"}, 
         {"name": "Happy Holidays",
-         "snippet": "Hello from the north pole, We hope you're all having happy holidays"},
+         "snippet": "Hello from the north pole, We hope you're all having happy holidays",
+         "pic": "hear_no"},
         {"name": "Special Request",  # the term 'Special Request' is special and will inject the text input field and more
          "snippet": "eg. Hello from Santa to the Robinson Family"},
     ],
     "Enjoy Family": [
         {"name": "Grandmas",
-         "snippet": "I hope you're having a wonderful time at Grandma's"},
+         "snippet": "I hope you're having a wonderful time at Grandma's",
+         "pic": "cough"},
         {"name": "Meals",
-         "snippet": "I hope you enjoyed your turkey dinner"},
+         "snippet": "I hope you enjoyed your turkey dinner",
+         "pic": "goofy"},
     ],
     "Storytime": [
         {"name": "Night Before Christmas",
-         "snippet": "Twas the night before Christmas and all through the house..."},
+         "snippet": "Twas the night before Christmas and all through the house...",
+         "pic": "book"},
         {"name": "Santa's Christmas Adventures",
-         "snippet": "Have you heard of hte great Forest of Burzee"}
+         "snippet": "Have you heard of hte great Forest of Burzee",
+         "pic": "phone"}
     ],
     # Some Goodness section could be good here. eg 'I have heard you have been good boys and girls'
     # also a good section to get custom moneys as you can say their actual names!!
     "Presents": [
         {"name": "Garage",
-         "snippet": "I left you some presents in the garage"},
+         "snippet": "I left you some presents in the garage",
+         "pic": "see_no"},
         {"name": "Kitchen",
-         "snippet": "I left you some presents in the kitchen"},
+         "snippet": "I left you some presents in the kitchen",
+         "pic": "hear_no"},
         {"name": "Upstairs",
-         "snippet": "I left you some presents upstairs"},
+         "snippet": "I left you some presents upstairs",
+         "pic": "hear_no"},
         {"name": "Downstairs",
-         "snippet": "I left you some presents downstairs"},
+         "snippet": "I left you some presents downstairs",
+         "pic": "point"},
         {"name": "Tree",
-         "snippet": "I left you some presents under the tree"},
+         "snippet": "I left you some presents under the tree",
+         "pic": "what"},
     ]
 }
 
